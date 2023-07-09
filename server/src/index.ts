774f08f69ec77099370c988659b03d1f96d2d389
yhhan <hyh123456@gmail.com>
2023-07-09 20:45:14 +0900

    postgres 연결

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,20 @@
-import express from "express";
-import morgan from "morgan";
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-const app = express();
+AppDataSource.initialize().then(async () => {
 
-app.use(express.json());
-app.use(morgan("dev"));
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
-app.get("/", (_, res) => res.send("running"));
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
 
-let port = 4000;
+    console.log("Here you can setup and run express / fastify / any other framework.")
 
-app.listen(port, async() => {
-    console.log("Server running at http://localhoste:${port}");
-});
-
+}).catch(error => console.log(error))
---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (_, res) => res.send("running"));
+
+let port = 4000;
+
+app.listen(port, async () => {
+  console.log(`Server running at http://localhoste:${port}`);
+
+  AppDataSource.initialize()
+    .then(async () => {
+        console.log("Here you can setup and run express / fastify / any other framework.");
+    })
+    .catch((error) => console.log(error));
+});
+
